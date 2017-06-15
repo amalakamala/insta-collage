21f835e38de5ddaee4ce394775da4072c80a4384
amalakamala <amalakamala.a@gmail.com>
2017-06-15 17:28:07 -0400

    dog

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,26 @@ boton.addEventListener("click", function(){
 		elBoton.setAttribute("href","gallery.html");
 	}
 
-
-
-
 })
+
+
+
+function drag(ev){ //ev de event es lo mismo, los atributos pueden variar
+	ev.dataTransfer.setData('text', ev.target.id);
+	//img se lee como text en el navegador
+	//target es el objetivo 
+}
+
+
+function finalDrop(ev){
+	ev.preventDefault(); 
+	//SIEMPRE se pondrá esto 
+
+}
+
+function drop(ev){
+	ev.preventDefault();
+	var dato = ev.dataTransfer.getData('text');
+	//getData trae.. sin el ev.target.id
+	ev.target.appendChild(document.getElementById(dato));	
+}
